cd36bc5d5750a279dc26ef3666272f6829809249
Nasya_K <nastenka05m@gmail.com>
2022-12-06 17:47:14 +0200

    Now it's only 3 words

--- ConsoleApplication15.cpp
+++ ConsoleApplication15.cpp
@@ -11,9 +11,9 @@ void NumIsTrue (const char Word[])
     int k = 0;
     for (int n = 0; n <= strlen(Word); n++)
     {
-        for (int mm = 48; mm <= 57; mm++)
+        for (int count = 48; count <= 57; count++) //char (48, 49..57) == numbers
         {
-            if (Word[n] == ((char)mm))
+            if (Word[n] == ((char)count))
             {
                 k++;
             }
@@ -28,11 +28,6 @@ int main()
     cout << " I will find the words with only 1 number\n";
     cout << " You have 20 symbols for ENGLISH Word \n";
     cout << "\nEnter 1st Word\t";
-    //Можна використовувати 
-    // setlocale(LC_ALL, "ukr"); 
-    // або SetConsoleCP(1251);
-    //SetConsoleOutputCP(1251);
-    //Але для спрощення усе англійскою
     char firstWord[20];
     cin >> firstWord;
     NumIsTrue(firstWord);
@@ -44,12 +39,4 @@ int main()
     char thirdWord[20];
     cin >> thirdWord;;
     NumIsTrue(thirdWord);
-    cout << "\nEnter 4 Word\t";
-    char fourWord[20];
-    cin >> fourWord;
-    NumIsTrue(fourWord);
-    cout << "\nEnter 5 Word \t";
-    char fiveWord[20];
-    cin >> fiveWord;
-    NumIsTrue(fiveWord);
 }
